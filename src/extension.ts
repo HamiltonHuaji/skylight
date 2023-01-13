602ffc17dd4aff7c88c34f85f317f23350253790
HamiltonHuaji <39890671+HamiltonHuaji@users.noreply.github.com>
2023-01-13 20:43:27 +0800

    ...

--- src/extension.ts
+++ src/extension.ts
@@ -3,27 +3,77 @@
 import * as vscode from 'vscode';
 import { LightSensor } from '@nodert-win10-19h1/windows.devices.sensors';
 
+
+function checkIlluminance(sensor: LightSensor) {
+    let reading = sensor.getCurrentReading().illuminanceInLux;
+    vscode.window.showInformationMessage(sensor.getCurrentReading().illuminanceInLux.toString());
+    if (reading > 100) {
+        // use light theme
+    } else {
+        // use dark theme
+    }
+}
+
+let interval: number = 1000;
+let sensor = LightSensor.getDefault();
+let handler: number | null = null;
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "skylight" is now active!');
+    // console.log('Congratulations, your extension "skylight" is now active!');
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('skylight.helloWorld', () => {
+    const activeCommandId = 'skylight.active';
+    const deactiveCommandId = 'skylight.deactive';
+    const toggleCommandId = 'skylight.toggle';
+
+    let activateCommand = vscode.commands.registerCommand(activeCommandId, () => {
         // The code you place here will be executed every time your command is executed
         // Display a message box to the user
-        const sensor = LightSensor.getDefault();
-        vscode.window.showInformationMessage(sensor.getCurrentReading().illuminanceInLux.toString());
-        // vscode.window.showInformationMessage(Object.keys(LightSensor).join(", "));
+        handler = setInterval(() => {
+            checkIlluminance(sensor);
+        }, 1000);
+    });
+    let deactivateCommand = vscode.commands.registerCommand(deactiveCommandId, () => {
+        // The code you place here will be executed every time your command is executed
+        // Display a message box to the user
+        if (handler !== null) {
+            clearInterval(handler);
+            handler = null;
+        }
+    });
+    let toggleCommand = vscode.commands.registerCommand(toggleCommandId, () => {
+        // The code you place here will be executed every time your command is executed
+        // Display a message box to the user
+        if (handler !== null) {
+            clearInterval(handler);
+            handler = null;
+        } else {
+            handler = setInterval(() => {
+                checkIlluminance(sensor);
+            }, 1000);
+        }
     });
 
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(activateCommand);
+    context.subscriptions.push(deactivateCommand);
+    context.subscriptions.push(toggleCommand);
+
+    let statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+    statusBarItem.command = toggleCommandId;
+    context.subscriptions.push(statusBarItem);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {
+    if (handler !== null) {
+        clearInterval(handler);
+        handler = null;
+    }
+}
